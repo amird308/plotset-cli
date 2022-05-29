8835b71e78bca01a0902f805afc2453cc8b2bb3b
Amir damir <amird308@gmail.com>
2022-05-29 15:44:28 +0430

    base chart template

---
+++ src/templates/chart/src/js/chart.js
@@ -0,0 +1,115 @@
+let option
+let chart
+let chartDom
+
+const updateOptionsStyles = () => {
+  const options = {
+    backgroundColor: config?.chart?.background,
+    title: {
+      show: config?.title?.show,
+      text: config?.title?.text,
+      subtext: config?.subtext?.text,
+      left: config?.title?.align,
+      textStyle: {
+        textBorderType: config?.title?.textBorderType,
+        color: config?.title?.color
+      },
+      subtextStyle: {
+        color: config?.subtext?.color
+      }
+    },
+    tooltip: {
+      trigger: 'item'
+    },
+    legend: {
+      show: config?.legend?.show,
+      orient: config?.legend?.orient,
+      left: config?.legend?.align,
+      padding: Number(config?.legend?.padding),
+      itemGap: Number(config?.legend?.itemGap)
+    },
+    series: [
+      {
+        roseType: stringToBoll(config?.chart?.type),
+        radius: [
+          `${config?.chart?.internalRadius}%`,
+          `${config?.chart?.externalRadius}%`
+        ],
+        avoidLabelOverlap: true,
+        itemStyle: {
+          borderRadius: config?.chart?.borderRadius,
+          borderColor: config?.chart?.borderColor,
+          borderWidth: config?.chart?.borderWidth
+        },
+        emphasis: {
+          itemStyle: {
+            shadowBlur: 10,
+            shadowOffsetX: 0,
+            shadowColor: 'rgba(0, 0, 0, 0.5)'
+          }
+        }
+      }
+    ]
+  }
+  chart.setOption(options)
+}
+
+const init_handler = () => {
+  chartDom = document.getElementById('chart-wrapper')
+  chart = echarts.init(chartDom)
+  option = {
+    dataset: {
+      source: data
+    },
+    series: [
+      {
+        type: 'pie',
+        encode: {
+          value: col_rel?.values,
+          itemName: col_rel?.labels
+        }
+      }
+    ]
+  }
+  chart.setOption(option, {
+    notMerge: true,
+    lazyUpdate: true
+  })
+}
+
+const transformData = async newData => {
+  const [...otherData] = newData
+  return otherData
+}
+
+window.addEventListener(
+  'load',
+  () => {
+    base_first_time({
+      _col_rel: { value: 'value', title: 'name' },
+      _config: {
+        'chart.title.color': '#111'
+      },
+      _data: [{ year: '1900', name: 'iran', value: '400' }]
+    })
+  },
+  false
+)
+
+const change_config_handler = () => {
+  if (!chart) return
+  updateOptionsStyles()
+}
+
+const resizeHandler = () => {
+  chart?.resize({
+    width: width,
+    height: height
+  })
+}
+module.exports = {
+  change_config_handler,
+  resizeHandler,
+  init_handler,
+  transformData
+}
---
+++ src/templates/chart/src/js/index.js
@@ -0,0 +1,8 @@
+import { init_handler, change_config_handler, resizeHandler, transformData } from './chart';
+
+
+
+window.init_handler = init_handler;
+window.change_config_handler = change_config_handler;
+window.resizeHandler = resizeHandler;
+window.transformData = transformData;
